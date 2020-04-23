94521539f0c457ea43a758afec26624c7932497c
kumisaki <binkumisaki@gmail.com>
2020-04-23 22:09:58 +0800

    add build folder

---
+++ build/precache-manifest.9a7aa5b8d8977dde516820357d60fd34.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e31d069ea93d7de443c4e8ad4d8a8869",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e538fc8548537ef8176",
+    "url": "/static/css/2.9f399191.chunk.css"
+  },
+  {
+    "revision": "1e538fc8548537ef8176",
+    "url": "/static/js/2.9e6e7921.chunk.js"
+  },
+  {
+    "revision": "a898738b4d7b39f735c6",
+    "url": "/static/js/main.8a94cd1c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
